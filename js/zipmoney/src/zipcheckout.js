0fc28610f32cb51b0e277c9f00f499fc1c2bea2c
Sagar Bhandari <sagar.bhandari@zipmoney.com.au>
2017-05-03 17:05:57 +1000

    Support for Magestore onestepcheckout

--- js/zipmoney/src/zipcheckout.js
+++ js/zipmoney/src/zipcheckout.js
@@ -1,3 +1,12 @@
+/**
+ * @category  Zipmoney
+ * @package   Zipmoney_ZipmoneyPayment
+ * @author    Sagar Bhandari <sagar.bhandari@zipmoney.com.au>
+ * @copyright 2017 zipMoney Payments Pty Ltd.
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.zipmoney.com.au/
+ */
+
 var zipCheckout = Class.create();
 
 zipCheckout.prototype = {
