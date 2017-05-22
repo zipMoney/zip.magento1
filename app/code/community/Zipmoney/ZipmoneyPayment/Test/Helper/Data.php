d554b7f60554edadae430778df5b8b44891902bc
Sagar Bhandari <sagar.bhandari@zipmoney.com.au>
2017-05-22 11:13:52 +1000

    Version Bump

--- app/code/community/Zipmoney/ZipmoneyPayment/Test/Helper/Data.php
+++ app/code/community/Zipmoney/ZipmoneyPayment/Test/Helper/Data.php
@@ -42,7 +42,7 @@ class Zipmoney_ZipmoneyPayment_Test_Helper_Data extends EcomDev_PHPUnit_Test_Cas
    * @test
    */
   public function testVersion(){
-    $this->assertEquals($this->_data->getExtensionVersion(),"1.0.0-rc9");
+    $this->assertEquals($this->_data->getExtensionVersion(),"1.0.0-rc10");
   }
   
 }
