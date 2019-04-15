a25c8afaafadf21411317e4d4e54f5d975c4c3e9
henryfan@zip <haolong.fan@zip.co>
2019-04-15 14:21:07 +1000

    Update deploy post

--- build/shell/deploy-post.sh
+++ build/shell/deploy-post.sh
@@ -7,13 +7,24 @@
 FILE_NAME='composer.json'
 
 # get release version
-RELEASE_VERSION_NUMBER=$(cat $FILE_NAME | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g');
-RELEASE_VERSION="v${RELEASE_VERSION_NUMBER}"
+RELEASE_VERSION=$(cat $FILE_NAME | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g');
 
 # Add release tag
-git tag -a ${RELEASE_VERSION} -m "Releasing version ${RELEASE_VERSION}" 
+git tag -a ${RELEASE_VERSION} -m "Releasing version ${RELEASE_VERSION}"
 git push origin ${RELEASE_VERSION}
 
 # create release branch
-git branch release/${RELEASE_VERSION_NUMBER}
-git push origin release/${RELEASE_VERSION_NUMBER}
+git branch release/${RELEASE_VERSION}
+git push origin release/${RELEASE_VERSION}
+
+##########################################################
+## Deploy code to the Github
+##########################################################
+
+GITHUB_REPO="https://github.com/zipMoney/zip.magento1"
+
+# curl -u ${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN} https://api.github.com/user
+git remote add github ${GITHUB_REPO}
+git push github master
+git push github release/${RELEASE_VERSION}
+git push --tags github
