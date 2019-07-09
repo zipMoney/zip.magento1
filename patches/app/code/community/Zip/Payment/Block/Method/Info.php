fe52f3baec2c7a85f15bd57fc601437da4b3bd92
ben.zhang <ben.zhang@zip.co>
2019-07-09 10:52:01 +1000

    adding the product type in payment info

---
+++ patches/app/code/community/Zip/Payment/Block/Method/Info.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Block model of checkout method information
+ *
+ * @package Zip_Payment
+ * @author  Zip Co - Plugin Team
+ **/
+
+class Zip_Payment_Block_Method_Info extends Mage_Payment_Block_Info
+{
+    const RECEIPT_NUMBER_LABEL = 'Receipt Number';
+    const PRODUCT_TYPE_LABEL = 'Product Type';
+
+    protected $_template = 'zip/payment/method/info/default.phtml';
+
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate($this->_template);
+    }
+
+    /**
+     * put receipt number into additional information
+     */
+    protected function _prepareSpecificInformation($transport = null)
+    {
+        $transport = parent::_prepareSpecificInformation($transport);
+        $payment = $this->getInfo();
+        $additionalInformation = $payment->getAdditionalInformation();
+
+        $info = array();
+
+        if (isset($additionalInformation[Zip_Payment_Model_Config::PAYMENT_RECEIPT_NUMBER_KEY])) {
+            $info[self::RECEIPT_NUMBER_LABEL] = $additionalInformation[Zip_Payment_Model_Config::PAYMENT_RECEIPT_NUMBER_KEY];
+        }
+        if (isset($additionalInformation['product'])) {
+            $info[self::PRODUCT_TYPE_LABEL] = $additionalInformation['product'];
+        }
+
+        return $transport->addData($info);
+    }
+
+    /**
+     * get log url
+     */
+    protected function getLogo()
+    {
+        return Mage::helper('zip_payment')->getConfig()->getLogo();
+    }
+}
